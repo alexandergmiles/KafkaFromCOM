c6753ff9025058c03bf1b57fec4eb282b6926731
unknown <Alexander Miles>
2020-07-27 20:15:08 +0100

    Initial commit

---
+++ KafkaStreams/COMToKafka.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+
+namespace KafkaStreams
+{
+    public class COMToKafka
+    {
+        public SerialPort comPort { get; }
+        public bool IsPortOpen => comPort.IsOpen;
+        public COMToKafka(SerialPort port) => comPort = port; 
+
+        //We need to setup a handshake here that configures messages
+        //Becading is fun
+
+
+        /// <summary>
+        /// Reads from the COM port
+        /// </summary>
+        /// <returns></returns>
+        public string ReadFromCOM(string port)
+        {
+            //We know we're reading messages of 9600 bits
+            byte[] messageBuffer = new byte[9600];
+
+            //We want this because we want to keep track of the number of messages
+            double messageNumber = 0;
+
+            if(!comPort.IsOpen)
+            {
+                comPort.Open();
+            }
+
+            //Is the port open?
+            if (comPort.IsOpen)
+            {
+                //Get the  most recent message
+                var result = comPort.Read(messageBuffer, 0, 9600);
+                //Decode from byte array to string
+                var stuffToWrite = Encoding.UTF8.GetString(messageBuffer);
+
+                //Sleep for one second
+                System.Threading.Thread.Sleep(1000);
+
+                //Should probably setup a handshake thing
+
+                //Return the most recent message
+                return stuffToWrite;
+            }
+
+            return "";
+        }
+
+
+        /// <summary>
+        /// Does the monitoring for LUMENs and writes that back to the kafka stream
+        /// </summary>
+        public void BeginMonitoring()
+        {
+            //Write to kafka using kSQL
+        }
+    }
+}
---
+++ KafkaStreams/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using System.IO.Ports;
+
+namespace KafkaStreams
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Printing because duh
+            Console.WriteLine("Hello Kafka producer! We're going to be writing to Kafka!");
+
+            //Configuration object where 
+            var kafkaConfig = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            
+            //Create the port
+            SerialPort port = new SerialPort("COM3"); 
+            
+            //Create the observer
+            var comObserver = new COMToKafka(port);
+            
+            //While the port is
+            do
+            {
+                //Get the current message
+                var result = comObserver.ReadFromCOM("COM3");
+                
+                //Write the message
+                Console.WriteLine(result);
+            } while (comObserver.IsPortOpen);
+
+            ///Print to Kafka
+
+            //using (var p = new ProducerBuilder<Null, string>(kafkaConfig).Build())
+            //{
+            //    try
+            //    {
+            //        var dr = await p.ProduceAsync("topic-to-write-to", new Message<Null, string> { Value = "this is a test" });
+            //        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+            //    }
+            //    catch
+            //    {
+
+                //    }
+                //}
+        }
+    }
+}
